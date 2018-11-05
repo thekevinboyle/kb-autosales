47b882b387f0308f3ecf77446fce06df469c9edd
thekevinboyle <thekevinboyle@gmail.com>
2018-11-05 13:51:30 -0600

    replace placeholder images for cars and update titles

--- src/view/components/car-collections/index.js
+++ src/view/components/car-collections/index.js
@@ -8,36 +8,36 @@ export default function() {
             title: 'Honda Cars and SUVs',
             desc:
         'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.',
-            pic1: 'https://picsum.photos/700/700',
+            pic1: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/honda-01.png',
             title1: 'Honda Accord',
-            pic2: 'https://picsum.photos/800/800',
+            pic2: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/honda-02.png',
             title2: 'Honda Prius',
         },
         {
-            title: 'Honda Cars and SUVs',
+            title: 'Audi Cars and SUVs',
             desc:
         'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.',
-            pic1: 'https://picsum.photos/700/700',
+            pic1: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/audi-01.png',
             title1: 'Honda Accord',
-            pic2: 'https://picsum.photos/800/800',
+            pic2: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/audi-02.png',
             title2: 'Honda Prius',
         },
         {
-            title: 'Honda Cars and SUVs',
+            title: 'Chevy Cars, Trucks, and SUVs',
             desc:
         'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.',
-            pic1: 'https://picsum.photos/700/700',
+            pic1: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/chevy-01.png',
             title1: 'Honda Accord',
-            pic2: 'https://picsum.photos/800/800',
+            pic2: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/chevy-02.png',
             title2: 'Honda Prius',
         },
         {
-            title: 'Honda Cars and SUVs',
+            title: 'Ford Cars, Trucks, and SUVs',
             desc:
         'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.',
-            pic1: 'https://picsum.photos/700/700',
+            pic1: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/ford-01.png',
             title1: 'Honda Accord',
-            pic2: 'https://picsum.photos/800/800',
+            pic2: 'https://s3-us-west-2.amazonaws.com/mockup-homework-assets/ford-02.png',
             title2: 'Honda Prius',
         },
     ];
